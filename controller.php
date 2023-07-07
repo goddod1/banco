cebe36b7880aeab8421298b32ae5c492ca5913cb
goddod1 <132653795+goddod1@users.noreply.github.com>
2023-07-07 17:11:52 +0700

    init code
    add

--- controller.php
+++ controller.php
@@ -1,42 +1,73 @@
 <?php
-class quanco{
+
+class Quanco{
     public $color;
     public $name;
-    public $location;
-    public $move;
+    public $locationX;
+    public $locationY;
+
+    public function move(){
+
+    }
+
+    function set_name($name) {
+        $this->name = $name;
+      }
+      function get_name() {
+        return $this->name;
+      }
+      function set_color($color) {
+        $this->color = $color;
+      }
+      function get_color() {
+        return $this->color;
+      }
+      function set_locationX($locationX) {
+        $this->locationX = $locationX;
+      }
+      function get_locationX() {
+        return $this->locationX;
+      }
+      function set_locationY($locationY) {
+        $this->locationY = $locationY;
+      }
+      function get_locationY() {
+        return $this->locationY;
+      }
+
 
 }
-class xe extends quanco {
+class Xe extends Quanco {
     public function(){
 
     }
 }
 
-class ma extends quanco{
+class Ma extends Quanco{
     public function(){
 
     }
 }
 
-class tuong extends quanco{
+class Tinh extends Quanco{
     public function(){
 
     }
 }
 
-class hau extends quanco{
+class Hau extends Quanco{
     public function(){
 
     }
 }
 
-class tuong extends quanco{
+class Tuong extends Quanco{
     public function(){
 
     }
 }
 
-class tot extends quanco{
+class Tot extends Quanco{
     public function(){
 
     }
