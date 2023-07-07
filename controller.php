f0878c82ea8bd7be36081cb4f90f9454aae9c9e1
goddod1 <132653795+goddod1@users.noreply.github.com>
2023-07-07 16:45:46 +0700

    init code
    banco

---
+++ controller.php
@@ -0,0 +1,44 @@
+<?php
+class quanco{
+    public $color;
+    public $name;
+    public $location;
+    public $move;
+
+}
+class xe extends quanco {
+    public function(){
+
+    }
+}
+
+class ma extends quanco{
+    public function(){
+
+    }
+}
+
+class tuong extends quanco{
+    public function(){
+
+    }
+}
+
+class hau extends quanco{
+    public function(){
+
+    }
+}
+
+class tuong extends quanco{
+    public function(){
+
+    }
+}
+
+class tot extends quanco{
+    public function(){
+
+    }
+}
+?>
